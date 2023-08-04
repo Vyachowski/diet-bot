95dd8ee5a6fc021fbefd01e080ee1e074ff16743
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-08-04 10:58:28 +0300

    reafctor: parser to a class

--- src/parser.js
+++ src/parser.js
@@ -1,30 +1,31 @@
 import fs from "node:fs";
 
-function readJsonFile(filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const jsonObject = JSON.parse(fileContent, (key, value) => {
-      const parsedValue = parseFloat(value);
-      return !isNaN(parsedValue) ? parsedValue : value;
-    });
-    return jsonObject;
-  } catch (error) {
-    console.error('Ошибка чтения файла или преобразования JSON:', error);
-    throw error;
-    }
-}
+export default class Connector {
+  constructor(dataSource = 'json') {
+    this.dataSource = dataSource;
+  }
 
-function writeJsonFile(filePath, data) {
-  try {
-    const jsonData = JSON.stringify(data, null, 2);
-    fs.writeFileSync(filePath, jsonData, 'utf8');
-  } catch (error) {
-    console.error('Ошибка при записи файла:', error);
-    throw error;
-    }
-}
+  static readJsonFile(filePath) {
+    try {
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const jsonObject = JSON.parse(fileContent, (key, value) => {
+        const parsedValue = parseFloat(value);
+        return !isNaN(parsedValue) ? parsedValue : value;
+      });
+      return jsonObject;
+    } catch (error) {
+      console.error('Error reading file or converting JSON:', error);
+      throw error;
+      }
+  }
 
-export {
-  readJsonFile,
-  writeJsonFile
-};
+  static writeJsonFile(filePath, data) {
+    try {
+      const jsonData = JSON.stringify(data, null, 2);
+      fs.writeFileSync(filePath, jsonData, 'utf8');
+    } catch (error) {
+      console.error('Error writing file:', error);
+      throw error;
+      }
+  }
+}
