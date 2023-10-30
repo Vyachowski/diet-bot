26de5c10b060397b8be9f07b1a19145eb8ba1e2f
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-10-30 23:31:04 +0200

    feat: add perfectionist and data module folder

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
+import perfectionistLineLength from 'eslint-plugin-perfectionist/configs/recommended-line-length'
+import eslintConfigPrettier from "eslint-config-prettier";
+import perfectionist from 'eslint-plugin-perfectionist';
 import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
 import { fileURLToPath } from "url";
+import path from "path";
+
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,10 +12,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname, // optional; default: process.cwd()
   resolvePluginsRelativeTo: __dirname, // optional
 });
-import eslintConfigPrettier from "eslint-config-prettier";
+
 // noinspection JSUnusedGlobalSymbols
 export default [
   ...compat.extends("airbnb-base"),
+  perfectionistLineLength,
   eslintConfigPrettier,
   {
     files: ["src/**/*.js"],
@@ -22,11 +27,14 @@ export default [
     ignores:
       [
         "**/*.config.js",
-        "!**/eslint.config.js",
         "node_modules/*",
       ],
+    plugins: {
+      perfectionist,
+    },
     rules: {
-      semi: "error"
+      semi: "error",
+      'perfectionist/sort-interfaces': 'error',
     },
   }
 ];
