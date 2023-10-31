2e1d6e1561fa593708b4380e370145f0984cdc15
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-10-31 15:08:55 +0200

    feat: add basic database functions

---
+++ data-module/database.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+// function connectToMongoDb() {
+//   mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/diet-bot?retryWrites=true&w=majority`)
+//     .then(() => console.log('Connected successfully.'));
+// }
+// function disconnectFromMongoDb() {
+//   mongoose.disconnect().then(() => console.log('Disconnected successfully.'))
+// }
+//
+// export {
+//   connectToMongoDb,
+//   disconnectFromMongoDb
+// }
+
+class Database {
+  constructor() {
+    this._connectionUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/diet-bot?retryWrites=true&w=majority`;
+    this.mealTypes = ['breakfast', 'snack', 'lunch', 'dinner'];
+    this.connect(); // auto-connect on creating instance
+  }
+
+  initModels() {
+    const mealSchema = new mongoose.Schema({
+      name: String,
+      type: Array,
+      ingredients: Array,
+      cooking: Array,
+    });
+    this.mealModel = mongoose.model('Meal', mealSchema);
+  }
+
+  connect() {
+    mongoose.connect(this._connectionUri)
+      .then(() => {
+        console.log('Connected to the database');
+        this.initModels();
+      })
+      .catch((error) => {
+        console.error('Error connecting to the database: ', error);
+      });
+  };
+
+  disconnect() {
+    mongoose.disconnect()
+      .then(() => {
+        console.log('Disconnected from the database');
+      })
+      .catch((error) => {
+        console.error('Error disconnecting from the database: ', error);
+      })
+  };
+
+  getRandomMealByType(mealType) {
+    if (!this.mealTypes.includes(mealType)) {
+      throw new Error('Select one of the valid meal types: breakfast, snack, lunch, dinner');
+    }
+
+    let mealsByType = [];
+    this.mealModel.find({ type: mealType}, (error, result) => {
+      if (error) {
+        console.error('Error fetching data from the database: ', error)
+      } else {
+        console.log('All breakfasts in the database:', result)
+        mealsByType = result;
+      }
+    })
+
+    const randomMealNumber = Math.floor(Math.random() * mealsByType.length);
+    return mealsByType[randomMealNumber];
+  }
+}
+export default Database;
