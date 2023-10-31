baef3edd5798a5389c5ec45a8e30e6d35c1466da
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-10-31 15:11:14 +0200

    refactor: delete excess comment

--- data-module/database.js
+++ data-module/database.js
@@ -1,18 +1,5 @@
 import mongoose from "mongoose";
 
-// function connectToMongoDb() {
-//   mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/diet-bot?retryWrites=true&w=majority`)
-//     .then(() => console.log('Connected successfully.'));
-// }
-// function disconnectFromMongoDb() {
-//   mongoose.disconnect().then(() => console.log('Disconnected successfully.'))
-// }
-//
-// export {
-//   connectToMongoDb,
-//   disconnectFromMongoDb
-// }
-
 class Database {
   constructor() {
     this._connectionUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/diet-bot?retryWrites=true&w=majority`;
