c8c16fe5b9dee1766184df7470bba6013b9c387b
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-02 14:53:28 +0200

    feat: add main logic functionality – getNewMenu

---
+++ bin/index.js
@@ -0,0 +1,20 @@
+import Database from "../data-module/Database.js";
+
+// Connect to the database
+const db = new Database();
+await db.connect();
+
+// Create a new random menu
+async function getRandomMenu() {
+  const meals = db.meals;
+  let newMenu = {};
+
+  for (const meal of meals) {
+    newMenu[meal] = await db.getRandomMealByType(meal);
+  }
+
+  return newMenu;
+}
+
+getRandomMenu().then(r => console.log(Object.entries(r)));
+
