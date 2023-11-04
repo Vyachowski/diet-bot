fa1fa209ee336afa13522e50745316b5d0069899
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-11-04 13:38:07 +0200

    refactor: change index with develop version

--- bin/index.js
+++ bin/index.js
@@ -1,191 +1,58 @@
-import {
-  getCurrentTime,
-  hasPassedGivenDays,
-  mergeAndSumObjects,
-  multiplyObjectValues,
-  objectToTextColumn,
-} from "../src/functions.js";
-import Connector from "../src/connector.js";
+import Database from "../data-module/Database.js";
+// import basicMenu from "../data-module/basicMenu.js";
 
 class Diet {
-  constructor(user_id) {
-    this._connector = new Connector(user_id);
-    this._menuDuration = 3;
-    if (this._connector.dataSource === 'json') {
-      this._dishes = this._connector.getDishes();
-      this._ingredients = this._connector.getIngredients();
-      this._config = this._connector.getUserConfig();
+  constructor(menuDuration = 3, kitchenEquipment = ['multiCooker']) {
+    this.menuDuration = menuDuration;
+    this.kitchenEquipment = kitchenEquipment;
+    this.db = new Database();
+  }
+
+  // MENU API
+  async createRandomMenu() {
+    const meals = this.db.meals;
+    let newMenu = {};
+
+    for (const meal of meals) {
+      newMenu[meal] = await this.db.getRandomDish(meal);
     }
-  };
+    await this.db.setMenu(newMenu);
 
-  static getRandomMeal(mealVariants) {
-    if (mealVariants.length > 0) {
-      const index = Math.floor(mealVariants.length * Math.random());
-      const meal = mealVariants[index];
-      return meal;
+    return newMenu;
+  }
+
+  async getMenu() {
+    return await this.db.getMenu();
+  }
+
+  // GROCERY LIST API
+  async getGroceryListForMenu() {
+    const {breakfast, snack, lunch, afternoonSnack, dinner} = await this.db.getCurrentMenu();
+    const currentMenu = {breakfast, snack, lunch, afternoonSnack, dinner};
+    let notUniqueIngredients = [];
+    let uniqueIngredients;
+
+    for (const meal in currentMenu) {
+      notUniqueIngredients = notUniqueIngredients.concat(currentMenu[meal]['ingredients']);
     }
-    return null;
-  };
 
-  _createRandomMenu() {
-    return {
-      breakfast: Diet.getRandomMeal(this._dishes.breakfast),
-      snack: Diet.getRandomMeal(this._dishes.snack),
-      lunch: Diet.getRandomMeal(this._dishes.lunch),
-      dinner: Diet.getRandomMeal(this._dishes.dinner),
-    };
-  };
+    uniqueIngredients = notUniqueIngredients.reduce((accumulator, [ingredient, amount]) => {
+      accumulator[ingredient] = (accumulator[ingredient] || 0) + amount;
+      return accumulator;
+    }, {});
 
-  //  [['banana', 30], ['pineapple', 60]]
-  _createDish(dishName, ingredientsList) {
-    const ingredients = Object.fromEntries(ingredientsList);
-    return {"name": dishName, ingredients};
-  };
+    const uniqueIngredientsMultipliedByDietDuration = Object.entries(uniqueIngredients)
+      .map(([ingredient, value]) => [ingredient, (Number(value) * 3)]);
+    const groceryList = Object.fromEntries(uniqueIngredientsMultipliedByDietDuration);
 
-  _createIngredient(name, section) {
-    return { [name]: {"section": section} };
-  };  
+    console.log(uniqueIngredients);
+    console.log(uniqueIngredientsMultipliedByDietDuration);
+    console.log(groceryList);
+    // return uniqueIngredients;
+  }
 
-  _createIngredientsList() {
-    const breakfastIngredients = this._config.currentMenu.breakfast.ingredients;
-    const lunchIngredients = this._config.currentMenu.lunch.ingredients;
-    const snackIngredients = this._config.currentMenu.snack.ingredients;
-    const dinnerIngredients = this._config.currentMenu.dinner.ingredients;
-    const allIngredients = mergeAndSumObjects(
-      breakfastIngredients,
-      lunchIngredients,
-      snackIngredients,
-      dinnerIngredients
-    );
-    const ingredientsList = multiplyObjectValues(
-      allIngredients,
-      this._menuDuration
-    );
-    return ingredientsList;
-  };
-
-  _createGroceryList(ingredients) {
-    const allIngredientsList = Object.entries(this._ingredients);
-    const ingredientsList = ingredients;
-
-    const sections = [
-      ...new Set(
-        allIngredientsList.map(([, properties]) => properties.section)
-      ),
-    ];
-
-    const ingredientsBySection = sections.map((section) => {
-      const products = allIngredientsList
-        .filter(([, properties]) => properties.section === section)
-        .map(([product]) => product);
-      return { section, products };
-    });
-
-    const groceryList = ingredientsBySection.map(({ section, products }) => {
-      const productAmount = products
-        .filter((product) => ingredientsList[product] !== undefined)
-        .map((product) => ({ [product]: ingredientsList[product] }));
-      return { section, productAmount };
-    });
-
-    return groceryList;
-  };
-
-  setMenu() {
-    if (!hasPassedGivenDays(this._config.menuCreated, this._menuDuration)) {
-      return false;
-    }
-    try {
-      this._config.menuCreated = getCurrentTime();
-      this._config.currentMenu = this._createRandomMenu();
-      this._connector.setUserConfig(this._config);
-      return true;
-    } catch (error) {
-        console.error("Error while setting the menu:", error);
-    };
-  };
-
-  setGroceryList() {
-    try {
-      if (!hasPassedGivenDays(this._config.menuCreated, this._menuDuration)) {
-        const ingredientList = this._createIngredientsList();
-        this._config.currentGroceryList = this._createGroceryList(ingredientList);
-        this._connector.setUserConfig(this._config);
-        return true;
-      }
-      this.setMenu();
-      const ingredientList = this._createIngredientsList();
-      this._config.currentGroceryList = this._createGroceryList(ingredientList);
-      this._connector.setUserConfig(this._config);
-      return true;
-    } catch (error) {
-        console.error("Error while setting the grocery list:", error);
-    };
-  };
-
-  getMenuText() {
-    const menuData = {
-      "For breakfast · 🥓 · 🧇 · 🥞 · 🍳": this._config.currentMenu.breakfast,
-      "For snack · 🍎 · 🍪 · 🥨 · 🍫 · ": this._config.currentMenu.snack,
-      "For lunch · 🍽️ · 🥪 · 🍱 · 😋 ": this._config.currentMenu.lunch,
-      "For dinner · 🥘 · 🍲 · 🥣 · 🥗 ": this._config.currentMenu.dinner,
-    };
-
-    const menu = Object.entries(menuData).map(([mealName, dish]) =>
-        `| ${mealName}\n| ${dish.name.toUpperCase()}\n\n${objectToTextColumn(
-          dish.ingredients
-        )}\n\n`
-    );
-    const menuText = menu.join('');
-
-    return menuText;
-  };
-
-  getGroceryListText() {
-    const groceryListData = this._config.currentGroceryList;
-    const groceryListColumns = groceryListData.map(({ section, productAmount }) =>
-        `| ${section.toUpperCase()}\n\n${productAmount
-          .map((product) => objectToTextColumn(product))
-          .join("\n")}\n\n`
-        );
-    const groceryListText = groceryListColumns.join('');
-    return groceryListText;
-  };
-
-  getDishesText() {
-    const dishesData = {
-      "1️⃣ Breakfast dishes": this._dishes.breakfast,
-      "2️⃣ Snack dishes:": this._dishes.snack,
-      "3️⃣ Lunch dishes:": this._dishes.lunch,
-      "4️⃣ Dinner dishes:": this._dishes.dinner,
-      "🌟 Cheatmeal variants:": this._dishes.cheatmeal,
-    };
-
-    const dishes = Object.entries(dishesData).map(([meal, dishes]) =>
-        `${meal}\n${dishes.map(dish => dish.name).join(', ')}\n\n`
-    );
-    const dishesText = dishes.join('');
-    
-    return dishesText;
-  };
-
-  addDish(meal, name, ingredients) {
-    const currentDishesList = this._dishes;
-    const mealTypes = ['breakfast', 'dinner', 'lunch', 'snack', 'cheatmeal'];
-    const newDish = {'name': name, 'ingredients': ingredients};
-
-    if (mealTypes.includes(meal)) {
-      currentDishesList[meal].push(newDish);
-      this._connector.setDishes(currentDishesList);
-      return true;
-    } else {
-      throw Error('Not a valid meal type');
-    }
-  };
-};
+  // DISHES API
+  // Nothing is here, but will be...
+}
 
 export default Diet;
-
-// const diet = new Diet(1);
-// console.log(diet._createingredient('orange', 'fresh produce'));
-// diet.addDish('breakfast', 'Rice boiled hard boiled', {'rice': 100, 'water': 400, 'salt': 2});
